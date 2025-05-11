456c02796e89d7595f9e8b94e019f9752e05497c
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2025-05-11 18:12:51 +0500

    feat(core): add nginx to commitlint

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,7 +2,11 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-case': [2, 'always', 'lower-case'],
-    'scope-enum': [2, 'always', ['cd', 'ci', 'backend', 'frontend', 'core']],
-    'scope-empty': [2, 'never'],  
+    'scope-enum': [
+      2,
+      'always',
+      ['backend', 'frontend', 'nginx', 'cd', 'ci', 'core'],
+    ],
+    'scope-empty': [2, 'never'],
   },
 };
