1d0b975e3ff6670b1577131417bb7d79009cbc03
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2025-05-11 18:10:33 +0500

    feat(core): commit scope is required

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,8 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+    'scope-enum': [2, 'always', ['cd', 'ci', 'backend', 'frontend', 'core']],
+    'scope-empty': [2, 'never'],  
+  },
+};
