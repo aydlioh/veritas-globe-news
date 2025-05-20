d0fe92a60a3e842dfe404a8f245415428d991543
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2025-05-21 03:04:31 +0500

    fix(backend): update news dto

--- backend/src/news/dto/update-news.dto.ts
+++ backend/src/news/dto/update-news.dto.ts
@@ -1,20 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, IsUrl } from 'class-validator';
 
 export class UpdateNewsDto {
   @ApiProperty({
     example: 'Заголовок новости',
     description: 'Заголовок статьи новости',
+    required: false,
   })
   @IsNotEmpty({ message: 'Заголовок обязателен для заполнения' })
+  @IsOptional()
   @IsString({ message: 'Заголовок должен быть строкой' })
   title?: string;
 
   @ApiProperty({
     example: 'Это содержание статьи новости.',
     description: 'Содержание статьи новости',
+    required: false,
   })
   @IsNotEmpty({ message: 'Содержание обязательно для заполнения' })
+  @IsOptional()
   @IsString({ message: 'Содержание должно быть строкой' })
   content?: string;
 
@@ -23,6 +27,7 @@ export class UpdateNewsDto {
     description: 'URL для превью изображения',
     required: false,
   })
+  @IsOptional()
   @IsUrl({}, { message: 'URL превью должен быть валидным URL' })
   previewUrl?: string; // TODO: Или файл?
 }
